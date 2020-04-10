0c602863427f314a63595c2ef1f4136c93c521fd
Azzam Alkhaldi <azzamalkhaldi74@gmail.com>
2020-04-10 23:34:35 +0300

    images directories changed

--- js/user_dashboard.js
+++ js/user_dashboard.js
@@ -14,7 +14,7 @@ export default function buildDashboard(){
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="card" style="width: 18rem;">
                         <div class="card-icon">
-                            <img src="../images/Update-Profile.png" class="card-img-top" alt="Update Profile">
+                            <img src="images/Update-Profile.png" class="card-img-top" alt="Update Profile">
                         </div>
                         <div class="card-body">
                           <h5 class="card-title">Update Profile</h5>
@@ -26,7 +26,7 @@ export default function buildDashboard(){
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="card" style="width: 18rem;">
                         <div class="card-icon">
-                            <img src="../images/Search-Services.png" class="card-img-top" alt="Browse Available Services">
+                            <img src="images/Search-Services.png" class="card-img-top" alt="Browse Available Services">
                         </div>
                         <div class="card-body">
                           <h5 class="card-title">Browse Available Services</h5>
@@ -38,7 +38,7 @@ export default function buildDashboard(){
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="card" style="width: 18rem;">
                         <div class="card-icon">
-                            <img src="../images/Medical-Service.webp" class="card-img-top" alt="Request Medical Services">
+                            <img src="images/Medical-Service.webp" class="card-img-top" alt="Request Medical Services">
                         </div>
                         <div class="card-body">
                           <h5 class="card-title">Request a Service</h5>
@@ -51,7 +51,7 @@ export default function buildDashboard(){
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="card" style="width: 18rem;">
                         <div class="card-icon">
-                            <img src="../images/Service-Status.png" class="card-img-top" alt="• Track the status of sent request 
+                            <img src="images/Service-Status.png" class="card-img-top" alt="• Track the status of sent request 
                             ">
                         </div>
                         <div class="card-body">
@@ -64,7 +64,7 @@ export default function buildDashboard(){
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="card" style="width: 18rem;">
                         <div class="card-icon">
-                            <img src="../images/Cancel (1).png" class="card-img-top" alt="Cancel a Request">
+                            <img src="images/Cancel (1).png" class="card-img-top" alt="Cancel a Request">
                         </div>
                         <div class="card-body">
                           <h5 class="card-title">Cancel a Request</h5>
@@ -76,7 +76,7 @@ export default function buildDashboard(){
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="card" style="width: 18rem;">
                         <div class="card-icon">
-                            <img src="../images/Feedback.svg" class="card-img-top" alt=" Send a feedback about the service">
+                            <img src="images/Feedback.svg" class="card-img-top" alt=" Send a feedback about the service">
                         </div>
                         <div class="card-body">
                           <h5 class="card-title">Feedback</h5>
