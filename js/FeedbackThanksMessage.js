f33704b1eb49b56f30c14e5449c823851fddb6e6
AhmedAlzahrani <34316296+AhmedAlzahrani@users.noreply.github.com>
2020-05-04 05:51:23 +0300

    Feedback Thank note has been added

---
+++ js/FeedbackThanksMessage.js
@@ -0,0 +1,43 @@
+export default function buildFeedbackThanksMessage(){
+    
+    $(".main").empty();
+    
+
+    $(".main").append(`
+
+
+    <div class="container">
+    
+        <div class="row">
+          <div class="col-3"></div>
+      
+        
+          <div class="col-6">
+      
+            <!-- start -->
+      
+            <div class="jumbotron" style="margin-top: 3em; margin-bottom: 20em;">
+              <h3 class="text-center" >Your Feedback has been recieved</h3>
+              <br>
+              <h3 class="text-center" >Thank You For Your Feedback</h3>
+              
+            </div>
+          <!-- end  -->
+      
+          </div>
+      
+      
+      
+      
+          <div class="col-3"></div>
+        </div>
+      
+      
+      </div>
+  
+  
+  <!--  -->
+
+    
+    `)
+}
