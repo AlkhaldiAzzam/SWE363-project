a260133e003bc618867d141ca88eb7926923d187
Azzam Alkhaldi <azzamalkhaldi74@gmail.com>
2020-05-04 07:04:35 +0300

    ...

--- js/emailHasBeenSentPage.js
+++ js/emailHasBeenSentPage.js
@@ -1,7 +1,7 @@
 export default function buildEmailHasBeenSentPage(){
-    $('#main').empty();
+    $('.main').empty();
 
-    $('#main').append(`
+    $('.main').append(`
     
     <div class="container">
     
