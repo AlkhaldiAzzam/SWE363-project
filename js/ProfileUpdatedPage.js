284a1477f56400f60013ec73bdaa8bece482fcbf
AhmedAlzahrani <34316296+AhmedAlzahrani@users.noreply.github.com>
2020-05-04 03:51:25 +0300

    ProfileUpdated.js Has been added

---
+++ js/ProfileUpdatedPage.js
@@ -0,0 +1,45 @@
+
+
+export default function buildProfileUpdatedPage(){
+
+    $('#main').empty();
+
+    $('#main').append(`
+    
+    
+    <div class="container">
+    
+    <div class="row">
+      <div class="col-3"></div>
+  
+    
+      <div class="col-6">
+  
+        <!-- start -->
+  
+        <div class="jumbotron" style="margin-top: 3em; margin-bottom: 20em;">
+          <h3 class="text-center" >Your profiile has been successfully updated</h3>
+          
+        </div>
+      <!-- end  -->
+  
+      </div>
+  
+  
+  
+  
+      <div class="col-3"></div>
+    </div>
+  
+  
+  </div>
+  
+  
+  <!--  -->
+    
+    
+    `);
+
+
+
+}
